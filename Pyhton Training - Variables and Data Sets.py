d2db750366db0d20911488db8b32e3f1961b0a90
inboundgold <webmaster@inboundgoldusa.com>
2023-12-26 21:13:16 -0500

    Create Pyhton Training - Variables and Data Sets.py

---
+++ Pyhton Training - Variables and Data Sets.py
@@ -0,0 +1,9 @@
+character_name = "George"
+character_age = 70.5
+
+print("There once was a man named" + character_name +",");
+print("he was " + character_age + " years old.");
+print("He really liked the name " + character_name + ","); 
+print("but didn't like being ") + (character_age) + (".");
+# The above code will not work because the variable character_age is a float and not a string.
+
